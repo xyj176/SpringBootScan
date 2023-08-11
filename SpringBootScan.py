9aae09a6a231b6c978a81ab9ecff7884ff3fc57f
xuyj <xuyj@dist.com.cn>
2023-08-11 15:08:19 +0800

    feat:报告由txt改为html形式

--- SpringBootScan.py
+++ SpringBootScan.py
@@ -17,7 +17,10 @@ python3 SpringBootScan.py
 
 边学边写，所以所有的注释就不删除了
 '''
+from pathlib import Path
 
+import dominate
+from dominate.tags import *
 import requests
 import sys, os
 import threading  # 引入多线程
@@ -49,122 +52,118 @@ def run():
     time.sleep(1)
 
 
-def text_create():  # 创建报告文本
+def html_create():  # 创建报告文本
     global report_path
-    for line in open('url.txt'):
-        name = line
-        break
     now = int(time.time())  # 获取时间戳
     time_array = time.localtime(now)  # 格式化时间戳为本地的时间
     other_style_time = time.strftime("%H%M%S", time_array)  # 格式化时间，获取时分秒
-    report_path = 'result/' + name.split('.')[1] + other_style_time + '_report.txt'  # 获取文本第一行域名，并且添加时分秒为报告名字
+    report_path = 'result/' + other_style_time + '_report.html'  # 获取文本第一行域名，并且添加时分秒为报告名字
 
 
-def get_rule(r, url, report):
+def get_rule(r, url):
+    content = None
+    global count
     # 判断报告中是否已存在该URL
     if "OSS" in r.text:
+        count += 1
         print('\033[0;34m[%s]\033[0m \033[0;32m<SUSPECT>\033[0m' % r.status_code, '\033[0;35m%s\033[0m' % url,
               '\033[1;31m疑似存在阿里云OSS密钥信息泄漏，请自行确认\033[0m')
-        report.write('[%s] ' % r.status_code + url + ' 疑似存在阿里云OSS密钥信息泄漏，请自行确认' + '\n')
+        content = '[%s] ' % r.status_code + url + ' 疑似存在阿里云OSS密钥信息泄漏，请自行确认'
+        with doc:
+            a(content, href=url, target='_blank')
+            br()
     if 'title="%s"' % url in r.text:
+        count += 1
         print('\033[0;34m[%s]\033[0m \033[0;32m<SUSPECT>\033[0m' % r.status_code, '\033[0;35m%s\033[0m' % url,
               '\033[1;31m疑似存在springboot信息泄漏，请自行确认\033[0m')
-        report.write('[%s] ' % r.status_code + url + ' 疑似存在springboot信息泄漏，请自行确认' + '\n')
+        content = '[%s] ' % r.status_code + url + ' 疑似存在springboot信息泄漏，请自行确认'
+        with doc:
+            a(content, href=url, target='_blank')
+            br()
     if r.status_code == 200:
+        count += 1
         # 200的都是未鉴权的
         print('\033[0;34m[%s]\033[0m \033[0;32m<未鉴权>\033[0m' % r.status_code,
               '\033[0;33m%s\033[0m' % url)
-        report.write('[%s] ' % r.status_code + url + ' 未鉴权，请自行确认' + '\n')
+        content = '[%s] ' % r.status_code + url + ' 未鉴权，请自行确认'
+        with doc:
+            a(content, href=url, target='_blank')
+            br()
         if "swagger" in r.url:
+            count += 1
             print('\033[0;34m[%s]\033[0m \033[0;32m<SUSPECT>\033[0m' % r.status_code, '\033[0;35m%s\033[0m' % url,
                   '\033[1;31m疑似存在springboot信息泄漏，请自行确认\033[0m')
-            report.write('[%s] ' % r.status_code + url + ' 疑似存在springboot信息泄漏，请自行确认' + '\n')
+            content = '[%s] ' % r.status_code + url + ' 疑似存在springboot信息泄漏，请自行确认'
+            with doc:
+                a(content, href=url, target='_blank')
+                br()
         elif "actuator" in url:
+            count += 1
             print('\033[0;34m[%s]\033[0m \033[0;32m<SUSPECT>\033[0m' % r.status_code, '\033[0;35m%s\033[0m' % url,
                   '\033[1;31m疑似存在actuator信息泄漏，请自行确认\033[0m')
-            report.write('[%s] ' % r.status_code + url + ' 疑似存在actuator信息泄漏，请自行确认' + '\n')
+            content = '[%s] ' % r.status_code + url + ' 疑似存在actuator信息泄漏，请自行确认'
+            with doc:
+                a(content, href=url, target='_blank')
+                br()
     else:
         print('\033[0;34m[%s]\033[0m \033[0;32m<KEYWORD_NOT_FOUND>\033[0m' % r.status_code, '\033[0;33m%s\033[0m' % url)
+        # content = '[%s] ' % r.status_code + url
+        # with doc:
+        #     a(content, href=url, target='_blank')
+        #     br()
 
 
-# def getRule(r, url, report):
-#     # 判断报告中是否已存在该URL
-#     if "swagger" in r.url and r.status_code == 200:
-#         if "springfox-swagger-ui" in r.text or "springfox.css" in r.text:
-#             print('\033[0;34m[%s]\033[0m \033[0;32m<SUSPECT>\033[0m' % r.status_code, '\033[0;35m%s\033[0m' % url,
-#                   '\033[1;31m疑似存在springboot信息泄漏，请自行确认\033[0m')
-#             report.write(url + ' status_code:' + r.status_code + '\n')
-#         else:
-#             print('\033[0;34m[%s]\033[0m \033[0;32m<KEYWORD_NOT_FOUND>\033[0m' % r.status_code,
-#                   '\033[0;33m%s\033[0m' % url)
-#     elif "OSS" in r.text:
-#         print('\033[0;34m[%s]\033[0m \033[0;32m<SUSPECT>\033[0m' % r.status_code, '\033[0;35m%s\033[0m' % url,
-#               '\033[1;31m疑似存在阿里云OSS密钥信息泄漏，请自行确认\033[0m')
-#         report.write(url + ' status_code:' + r.status_code + '\n')
-#     elif 'title="%s"' % url in r.text:
-#         print('\033[0;34m[%s]\033[0m \033[0;32m<SUSPECT>\033[0m' % r.status_code, '\033[0;35m%s\033[0m' % url,
-#               '\033[1;31m疑似存在springboot信息泄漏，请自行确认\033[0m')
-#         report.write(url + ' status_code:' + r.status_code + '\n')
-#     elif "actuator" in url and r.status_code == 200:
-#         print('\033[0;34m[%s]\033[0m \033[0;32m<SUSPECT>\033[0m' % r.status_code, '\033[0;35m%s\033[0m' % url,
-#               '\033[1;31m疑似存在actuator信息泄漏，请自行确认\033[0m')
-#         report.write(url + ' status_code:' + r.status_code + '\n')
-#     else:
-#         print('\033[0;34m[%s]\033[0m \033[0;32m<KEYWORD_NOT_FOUND>\033[0m' % r.status_code, '\033[0;33m%s\033[0m' % url)
-
-
-def req(line1, line2, report):  # 发送请求
+def req(url, path):  # 发送请求
     r = None
     try:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/79.0.3945.88 Safari/537.36'}
         # if line1.startswith('https://'): #判断开头是否为https，若true则post
-        #    u = line1.replace('\n','')+line2
+        #    full_url = line1.replace('\n','')+line2
         #    requests.packages.urllib3.disable_warnings()#关闭警告
-        #    r = requests.post(u,headers=headers,allow_redirects=False,verify=False)证书校验关闭
-        #    getRule(r,u,report)#调用判断方法
+        #    r = requests.post(full_url,headers=headers,allow_redirects=False,verify=False)证书校验关闭
+        #    getRule(r,full_url,report)#调用判断方法
         # else:
-        for line in open('dic/app_name.txt'):
-            line = line.strip()
-            u = line1.replace('\n', '') + line.replace('\n', '')
-            if u.endswith('/'):
-                u = u[:-1]  # 去掉最后一个/
-                u = u + line2.replace('\n', '')
-            print(u)
+        for app_name in open('dic/app_name.txt'):
+            app_name = app_name.strip()
+            full_url = url.replace('\n', '') + app_name.replace('\n', '')
+            if full_url.endswith('/'):
+                full_url = full_url[:-1]  # 去掉最后一个/
+                full_url = full_url + path.replace('\n', '')
             try:
-                r = requests.get(u, headers=headers, allow_redirects=False)
+                r = requests.get(full_url, headers=headers, allow_redirects=False)
             except:
                 pass
             if r is not None:
-                get_rule(r, u, report)
+                get_rule(r, full_url)
     except:
-        print('\033[0;34m[%s]\033[0m \033[0;32m<ERROR>\033[0m \033[1;31m%s\033[0m' % (r.status_code, u))  # 输出异常信息
+        print(
+            '\033[0;34m[%s]\033[0m \033[0;32m<ERROR>\033[0m \033[1;31m%s\033[0m' % (r.status_code, full_url))  # 输出异常信息
         sys.exit()
 
 
-def get_url(line2):
-    report = open(report_path, 'a+')  # 追加输出记录，a+为可读可追加 w模式打开文件，如果而文件中有数据，再次写入内容，会把原来的覆盖掉
+def get_url(path):
     try:
         # 通过for循环拼接拿到所有请求url
-        for line1 in open('url.txt'):
-            line1 = line1.strip()  # 删除字符串首尾空格
-            if line1.endswith('/'):  # 判断传入的url是否携带/
+        for url in open('url.txt'):
+            url = url.strip()  # 删除字符串首尾空格
+            if url.endswith('/'):  # 判断传入的url是否携带/
                 # 携带则去除
-                req(line1.strip('/'), line2, report)  # 删除尾部/，strip只删除头尾部的关键字
+                req(url.strip('/'), path)  # 删除尾部/，strip只删除头尾部的关键字
             else:
-                req(line1, line2, report)
+                req(url, path)
     except:
         pass
-    finally:
-        # 发生错误时也要关闭文件，否则会出现文件占用的情况
-        report.close()
 
 
 if __name__ == '__main__':
     run()
-    report_path = 0
-    text_create()  # 程序启动时，先创建报告文件
+    count = 0  # 疑似的漏洞数量
+    report_path = 0  # 报告路径
+    html_create()  # 程序启动时，先创建报告文件
+    title = Path(report_path).stem
+    doc = dominate.document(title=title)
     try:
         # 多线程异步抓取
         # 根据敏感路径
@@ -178,12 +177,11 @@ if __name__ == '__main__':
         print('\033[0;34m[000]\033[0m \033[0;32m<ERROR>\033[0m  \033[1;31mThreadException\033[0m')
 
     try:
-        count = len(open(report_path, 'r').readlines())  # 获取报告中的行数
+        with open(report_path, 'w') as f:
+            f.write(doc.render())
         if count != 0:  # 检测是否存在漏洞
-            for i in open(report_path):  # 循环读取测试结果
-                print('\n\033[1;31m测试结束，共发现%s处疑似存在该漏洞的URL：\033[0m' % count)
-                print('\033[0;35m%s\033[0m' % i.replace('\n', ''))
-                print('\033[1;31m测试结果已保存至result文件夹下的%s文本中\033[0m' % report_path)
+            print('\n\033[1;31m测试结束，共发现%s处疑似存在该漏洞的URL：\033[0m' % count)
+            print('\033[1;31m测试结果已保存至result文件夹下的%s中\033[0m' % report_path)
         else:
             os.remove(report_path)  # 不存在则删除报告文件
             print('\n\033[1;31m测试结束，未发现疑似存在漏洞的URL\033[0m')
